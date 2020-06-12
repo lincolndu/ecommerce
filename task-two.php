d15671efcbd88fe10174351411b1216712e45ecd
Lincoln Mahmud <lincolndu@yahoo.com>
2020-06-12 20:28:59 +0600

    Update task-two.php

--- task-two.php
+++ task-two.php
@@ -1,13 +1,6 @@
 <?php 
 
-/*
-	*Task 2 File
-	*
-*/
-	
-/* 
-	*Include database connection file
-*/
+// Include database connection file
 
 include('database.php');
 
@@ -23,46 +16,39 @@ include('database.php');
     public function category_tree(){
 		 $sql='select c.Id as id, c.Name as name, case when cr.ParentcategoryId is null then 0 else cr.ParentcategoryId end parent, count(item.Id) as count from category c left join item_category_relations ir on ir.categoryId=c.Id left join item on item.Number=ir.ItemNumber left join catetory_relations cr on cr.categoryId=c.Id group by c.Id';
 		 $q = $this->conn->query($sql) or die("failed!");
-		 while($r = $q->fetchAll(PDO::FETCH_OBJ)){
-		 	$data[]=$r;
-		 }
-		 return $this->buildTree($data[0]);
+		 $data= $q->fetchAll(PDO::FETCH_ASSOC);
+		 return $this->buildTree($data);
 	}
 	/*
  		* Database data to tree making function
  	*/
-
-	public function buildTree($items) {
-	    $children = [];
-	    foreach($items as &$item) {
-	      $children[$item->parent][] = $item; 
-	      unset($item);
+ 	function buildTree(&$elements, $parentId = 0) {
+	    $branch = [];
+	    foreach ($elements as &$element) {
+	        if ($element['parent'] == $parentId) {
+	            $children = $this->buildTree($elements, $element['id']);
+	            if ($children) {
+	                $element['children'] = $children;
+	                $element['count'] += array_sum(array_column($children, 'count'));
+	            }
+	            $branch[$element['id']] = $element;
+	            unset($element);
+	        }
 	    }
-	    foreach($items as $item) {
-	      if(isset($children[$item->id])) {
-	      	$item->count= array_sum(array_column($children[$item->id],'count'));
-	        $item->children += $children[$item->id];
-	      }
-	    }
-	    foreach ($children[0] as $value) {
-	    	$value->count = array_sum(array_column($value->children,'count'));
-	    }
-	    return $children[0];
+	    return $branch;
 	}
-
 	/*
  		* Recursive loading data
  	*/
-
 	public function recursivePrint($data){
-		foreach ($data as $value) {
-			echo "<ul>";
-				echo "<li>" . $value->name . "(" .$value->count .')'. "</li>";
-				if (isset($value->children)) {
-					$this->recursivePrint($value->children);
-				}
-			echo "</ul>";	
+		echo "<ul>";
+		foreach ($data as $category) {			
+			echo '<li>' . $category['name'] . ' (' . $category['count'] . ')' . '</li>';
+			if (isset($category['children'])) {
+				$this->recursivePrint($category['children']);
+			}
 		}
+		echo "</ul>";
 	}	
  }
 
@@ -75,6 +61,7 @@ $obj= new databaseTaskTwo($connection->conn /*Connection data*/ );
 */
 $categories= $obj->category_tree();
 
+
 ?>
 
 <!DOCTYPE html>
@@ -83,30 +70,13 @@ $categories= $obj->category_tree();
 	<meta charset="UTF-8">
 	<title>Brain Station Assignment Task 2</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-	<style>
-		.root{}
-		.root li{}
-		.child_one{}
-		.child_one li{}
-		.child_two{}
-		.child_two li{}
-		.child_three li{}
-		.child_three{}
-	</style>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="sidebar">
-					<?php foreach($categories as $category ): ?>
-						<ul class="navbar root">
-							<li> <?=$category->name .' ('.$category->count.')';?>
-								<?php $obj->recursivePrint($category->children); ?>
-							</li>
-						</ul>
-					<?php endforeach; ?>
+					<?php $obj->recursivePrint($categories); ?>
 				</div>
 			</div>
 		</div>
